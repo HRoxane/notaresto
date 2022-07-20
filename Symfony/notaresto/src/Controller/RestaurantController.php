d87fa20a478f76a3a5aa3da8db8f6d887eede1d4
Roxane Houlgatte <roxane.houlgatte@live.fr>
2022-07-20 17:12:05 +0200

    Route restaurant controller

--- Symfony/notaresto/src/Controller/RestaurantController.php
+++ Symfony/notaresto/src/Controller/RestaurantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,70 @@ use Symfony\Component\Routing\Annotation\Route;
 class RestaurantController extends AbstractController
 {
     #[Route('/restaurant', name: 'app_restaurant')]
+    
+    
+    /**
+     * Afficher la liste des restaurants
+     * @Route("/restaurants", name="restaurant_index", methods="GET")
+     */
     public function index(): Response
     {
         return $this->render('restaurant/index.html.twig', [
             'controller_name' => 'RestaurantController',
         ]);
     }
+
+
+    /**
+     * Affiche un restaurant
+     * {restaurant} est un paramètre 
+     * @Route("restaurant/{restaurant}", name="restaurant_show", methods={"GET"}, requirements="restaurant"="\d+"})
+     * @param Restaurant $restaurant
+     */
+
+    public function show(Restaurant $restaurant){
+    }
+
+    /**
+     * Création d'un nouveau restaurant : afficher le formulaire
+     * @Route("restaurant/nex", name="restaurant_new", methods={"GET"})
+     */
+    public function new(){
+
+    }
+
+    /**
+     * Traite la reqsuête d'un formulaire de création de restaurant
+     * @Route("/restaurant", name="restaurant_create", methods={"POST"})
+     */
+    public function create(){
+
+    }
+
+    /**
+     * Affiche le forumaire d'édition d'un restaurant(GET)
+     * Traite le forumlaire d'édition d'un restaurant(POST) 
+     * @Route("/restaurant/{restaurant}/edit, name="restaurant_edit", methods={"GET", "POST"})
+     * @param Restaurant $restaurant
+     */
+
+     public function edit(Restaurant $restaurant){
+
+     }
+
+     /**
+      * Supprime un restaurant
+      * @Route("restaurant/{restaurant}/delete, name="restaurant_delete", methods={"DELETE"})
+      * @param Restaurant $restaurant
+      */
+
+      public function delete(Restaurant $restaurant) {
+        
+      }
+
+
+
+
+
+
 }
